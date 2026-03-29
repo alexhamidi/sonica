930425a1ea0cdf925f58b05897926c79efd58402
alex h <alextheastro@gmail.com>
2026-03-29 13:27:54 -0700

    updates

--- web/app/layout.tsx
+++ web/app/layout.tsx
@@ -7,7 +7,7 @@ import "./globals.css";
 const nunito = Nunito({
   variable: "--font-nunito",
   subsets: ["latin"],
-  weight: ["300", "400", "500", "600", "700", "800", "900"],
+  weight: ["300", "400", "500", "600", "700", "800"],
 });
 
 export const metadata: Metadata = {
